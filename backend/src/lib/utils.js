e81eedb1773e76196adefdff5c5362e1f1424fc0
Siddharthag2004 <gurrapusiddhartha2004@gmail.com>
2026-01-15 18:10:07 +0530

    Fix: Set sameSite to none for cross-origin cookies

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -12,14 +12,11 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks: cross-site scripting
-    sameSite: "strict", // CSRF attacks
-    secure: ENV.NODE_ENV === "development" ? false : true,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 Days in MS
+    httpOnly: true, // prevent XSS attacks
+    sameSite: "none", // <--- CHANGED: Allows cookie between Vercel & Render
+    secure: ENV.NODE_ENV !== "development", // <--- CHANGED: Ensures secure is TRUE in production
   });
 
   return token;
 };
-
-// http://localhost
-// https://dsmakmk.com
